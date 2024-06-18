8753171f7b5e48236d51d25c358786fefa0cdce1
krishd9895 <105974502+krishd9895@users.noreply.github.com>
2024-06-18 11:14:03 +0530

    Update logout.py

--- plugins/logout.py
+++ plugins/logout.py
@@ -21,15 +21,15 @@ async def revoke_handler(bot:Update, msg:Message):
         collection_login.delete_one(query)
 
         await msg.reply_text(
-            "Your account is now logged out🥺.\nTo Login again send your login detail.",
-            parse_mode = 'html'
+            "Your account is now logged out🥺.\nTo Login again send your login detail."
+            
         )
     
     # If user not found in db
     else:
         await msg.reply_text(
-            f"<b><u>You are not even logged in😒. So how can I remove your account.</u></b>{common_text}",
-            parse_mode = 'html'
+            f"<b><u>You are not even logged in😒. So how can I remove your account.</u></b>"
+            
         )
     return
 
